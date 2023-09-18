4c406ee307820e7cd76a90432f827b6319f2c901
IlhamStore23 <145270642+IlhamStore23@users.noreply.github.com>
2023-09-18 19:37:11 +0700

    Update hanyassh.sh

--- install/hanyassh.sh
+++ install/hanyassh.sh
@@ -3,7 +3,7 @@
 # 2022
 # ==================================================
 
-GitUser="syapik96"
+GitUser="IlhamStore23"
 #wget https://github.com/${GitUser}/
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
